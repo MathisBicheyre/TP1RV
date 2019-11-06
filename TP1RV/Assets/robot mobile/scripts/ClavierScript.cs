3d268bf6c624336fff956fb06d235e472518e3a5
PaulTranchet <paul.tranchet@efrei.net>
2019-11-06 10:23:34 +0100

    TP1 over

---
+++ TP1RV/Assets/robot mobile/scripts/ClavierScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClavierScript : MonoBehaviour
+{
+	public gere_bras gere_bras = null;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyUp(KeyCode.RightArrow))
+			gere_bras.increment.x += (float)0.01;
+		if (Input.GetKeyUp(KeyCode.LeftArrow))
+			gere_bras.increment.x -= (float)0.01;
+		if (Input.GetKeyUp(KeyCode.UpArrow))
+			gere_bras.increment.y += (float)0.01;
+		if (Input.GetKeyUp(KeyCode.DownArrow))
+			gere_bras.increment.y -= (float)0.01;
+		if (Input.GetKeyUp(KeyCode.A))
+			gere_bras.increment.z += (float)0.01;
+		if (Input.GetKeyUp(KeyCode.Z))
+			gere_bras.increment.z -= (float)0.01;
+	}
+}
