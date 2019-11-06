ef5462d5c718622947dce183b3a96a6b84777a21
Mathis <mathis.bicheyre@efrei.net>
2019-11-06 08:22:42 +0100

    Added TP1 elements

---
+++ TP1RV/elements_robot/script/camera_controller.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class camera_controller : MonoBehaviour {
+	
+	public Camera exocentrique1;
+	public Camera exocentrique2;
+	public Camera egocentrique;
+	private Camera[] liste;
+	private int courante;
+		
+	// Use this for initialization
+	void Start () {
+		exocentrique1.enabled = true;
+		exocentrique2.enabled = false;
+		egocentrique.enabled = false;
+		liste = new Camera[3];
+		liste[0] = exocentrique1;
+		liste[1] = exocentrique2;
+		liste[2] = egocentrique;
+		courante = 0;
+	}
+	
+	void Switch()
+	{
+		liste[courante%3].enabled = false;
+		courante++;
+		liste[courante%3].enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+		   Switch();
+		}
+	}
+}
